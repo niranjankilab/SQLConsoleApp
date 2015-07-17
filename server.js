88f2604790d9ef7933da37a232278c0567b98a22
niranjankilab <niranjank.sharma@gmail.com>
2015-07-16 20:39:15 -0700

    Added Server for Node Azure

--- server.js
+++ server.js
@@ -4,25 +4,8 @@ var connectionString = 'Driver={SQL Server Native Client 11.0};Server=tcp:niranj
 var testQuery = "SELECT 1";
 var awQuery = 'SELECT [EmailAddress] as emailid from [AdventureWorks].[SalesLT].[Customer]';
 
-sql.query(connectionString, awQuery, function (err, resultset) {
-    if (err)
-        console.log('Ohh Error');
-    else
-        console.log('Talking to Azure SQL');
-    console.log(resultset);
-});
-
-
-var port = 1337;
-var httpServer = http.createServer(function (req, res) {
+var port = process.env.PORT || 1337;
+http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write("node " + process.version + " " + process.arch + ".\n");
-    
-    sql.query(connectionString, awQuery, function (err, result) {
-        if (err)
-            res.end("Query Failed \n" + err);
-        else
-            var r = Math.floor((Math.random() * 100) + 1);
-        res.end("Query result: " + result[r].emailid + " \n");
-    });
-});
+    res.end('Hello World\n');
+}).listen(port);
